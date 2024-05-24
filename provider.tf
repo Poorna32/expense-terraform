bfd0bd51ad3fdd0355132f379da4694250a5a123
Poorna32 <poornachandraga44@gmail.com>
2024-05-24 14:05:45 +0530

    added 8200 and secret variable

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "vault" {
-  address         = "https://vault-internal.poornachandra3.online"
+  address         = "https://vault-internal.poornachandra3.online:8200"
   skip_tls_verify = true
   token           = var.vault_token
 }
