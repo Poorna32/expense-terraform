6b23ce6278bb94134ecdb550998fe2815c5dbebe
Poorna32 <poornachandraga44@gmail.com>
2024-06-17 10:43:27 +0530

    Create VPC.

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -2,3 +2,7 @@ env = "dev"
 instance_type= "t3.small"
 zone_id = "Z069282029EAN7Z8L8I3R"
 
+
+#VPC INPUTS
+vpc_cidr_block = "10.10.0.0/24"
+subnet_cidr_block = "10.10.0.0/24"
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,6 +1,3 @@
-variable "instance_type" {}
 variable "env" {}
-variable "zone_id" {}
-variable "vault_token" {}
 variable "vpc_cidr_block" {}
 variable "subnet_cidr_block" {}
