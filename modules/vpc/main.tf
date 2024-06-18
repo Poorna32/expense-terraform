ab50bbdb330e0fb10b231a47bd0ce088d5a3614b
Poorna32 <poornachandraga44@gmail.com>
2024-06-18 10:52:04 +0530

    Associate Subnets with Route Tables

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -55,6 +55,12 @@ resource "aws_route_table" "frontend" {
 }
 
 
+resource "aws_route_table_association" "frontend" {
+  count          = length(var.frontend_subnets)
+  subnet_id      = aws_subnet.frontend[count.index].id
+  route_table_id = aws_route_table.frontend[count.index].id
+}
+
 resource "aws_subnet" "backend" {
   count      = length(var.backend_subnets)
   vpc_id     = aws_vpc.main.id
@@ -78,6 +84,13 @@ resource "aws_route_table" "backend" {
   tags = {
     Name = "${var.env}-backend-rt-${count.index + 1}"
   }
+}
+
+
+resource "aws_route_table_association" "backend" {
+  count          = length(var.backend_subnets)
+  subnet_id      = aws_subnet.backend[count.index].id
+  route_table_id = aws_route_table.backend[count.index].id
 }
 
 resource "aws_subnet" "db" {
@@ -107,6 +120,14 @@ resource "aws_route_table" "db" {
 }
 
 
+
+resource "aws_route_table_association" "db" {
+  count          = length(var.db_subnets)
+  subnet_id      = aws_subnet.db[count.index].id
+  route_table_id = aws_route_table.db[count.index].id
+}
+
+
 resource "aws_subnet" "public" {
   count      = length(var.public_subnets)
   vpc_id     = aws_vpc.main.id
@@ -133,6 +154,12 @@ resource "aws_route_table" "public" {
   }
 }
 
+
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public[count.index].id
+}
 
 resource "aws_route" "default-vpc" {
   route_table_id            = var.default_route_table_id
