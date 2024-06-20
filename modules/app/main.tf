4cb12333443bdc2723595cd30ac8bee31a852b3d
Poorna32 <poornachandraga44@gmail.com>
2024-06-20 13:07:03 +0530

    Change Health Check for Target Groups

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -95,6 +95,15 @@ resource "aws_lb_target_group" "main" {
   port     = var.app_port
   protocol = "HTTP"
   vpc_id   = var.vpc_id
+
+  health_check {
+    healthy_threshold = 2
+    interval          = 5
+    path              = "/health"
+    port              = var.app_port
+    timeout           = 2
+    unhealthy_threshold = 2
+  }
 }
 
 resource "aws_lb_target_group_attachment" "main" {
@@ -105,7 +114,7 @@ resource "aws_lb_target_group_attachment" "main" {
 }
 
 
-resource "aws_lb_listener" "front_end" {
+resource "aws_lb_listener" "frontend" {
   count             = var.lb_needed ? 1 : 0
   load_balancer_arn = aws_lb.main[0].arn
   port              = var.app_port
