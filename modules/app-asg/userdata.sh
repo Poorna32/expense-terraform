188afa58fd898ee054d317782f3eaf6acddd663a
Poorna32 <poornachandraga44@gmail.com>
2024-07-15 13:31:57 +0530

    Using Golden AMI For APP setups.

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pip3.11 install ansible hvac &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/Poorna32/expense-ansible1 get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token}
 ansible-pull -i localhost, -U https://github.com/Poorna32/expense-ansible1 expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json &>>/opt/ansible.log
