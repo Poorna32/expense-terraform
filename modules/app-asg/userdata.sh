c6387de70366c39755078f8bb3a1a88efec4b435
Poorna32 <poornachandraga44@gmail.com>
2024-08-31 14:53:16 +0530

    Modified Pull Mechanism by adding ansible.log

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-ansible-pull -i localhost, -U https://github.com/Poorna32/expense-ansible1 get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token}
+ansible-pull -i localhost, -U https://github.com/Poorna32/expense-ansible1 get-secrets.yml -e env=${env} -e role_name=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/Poorna32/expense-ansible1 expense.yml -e env=${env} -e role_name=${component} -e @~/secrets.json  -e only_deployment=false &>>/opt/ansible.log
